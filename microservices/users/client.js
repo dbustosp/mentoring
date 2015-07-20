44aa929324b62d40d6430286e37664a78ecdfda3
dbustosp <dbustos10@gmail.com>
2015-07-19 23:46:30 -0300

    added method to get all users

--- microservices/users/client.js
+++ microservices/users/client.js
@@ -1,14 +1,19 @@
 var seneca = require('seneca')()
 
-//seneca.use('transport',{
-//  pins:[ {role:'math',cmd:'product'} ]
-//})
+//seneca
+//	.client()
+//	.act(
+//		{role:'math', cmd:'product', left:3, right:4},
+//		function(err, result) {
+//			if (err) return console.error(err)
+//			console.log(result)
+//		})
 
 seneca
 	.client()
-	.act( 
-		{role:'math', cmd:'product', left:3, right:4}, 
-        function(err,result) {
-        	if( err ) return console.error( err )
-            console.log(result)
-         })
+	.act(
+		{action: 'getAll'},
+		function(err, result) {
+			if (err) return console.error(err)
+			console.log(result)
+		})
