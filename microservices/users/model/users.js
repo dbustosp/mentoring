d83f4642d36058d83d6ed593f937259abcd964a0
dbustosp <dbustos10@gmail.com>
2015-07-19 23:46:05 -0300

    Added User model

---
+++ microservices/users/model/users.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var validate = require('mongoose-validator').validate;
+
+var userSchema = new mongoose.Schema({
+	name: {type: String, unique: true, required: true},
+	birthday: {type: String, unique: false, required: false},
+	startDate: {type: String, unique: false, required: false},
+	position: {type: String, unique: false, required: false},
+	username: {type: String, unique: false, required: false},
+	gender: {type: String, unique: false, required: false},
+	password: {type: String, unique: false, required: false},
+	email: {
+		type: String, 
+		unique: true,
+		required: false,
+	}
+});
+
+module.exports = mongoose.model('User', userSchema);
