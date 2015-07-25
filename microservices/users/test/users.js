6d2e471ae9f4d1c78ea1ea6471dc6ba48c0274ff
dbustosp <dbustos10@gmail.com>
2015-07-25 14:55:22 -0300

    Refactoring code

--- microservices/users/test/users.js
+++ microservices/users/test/users.js
@@ -1,57 +1,58 @@
 var assert = require('assert');
 var seneca = require('seneca')()
-	.error( assert.fail )
+	//.error( assert.fail )
 
 
-//get_users()
-//get_user()
+get_users()
+get_user()
 create_user()
 
-// Testing get users
 function get_users() {
 	seneca
 		.client()
 		.act(
 			{action: 'getAll'},
-			function( err, result) {
-				assert.equal( result.message.length, 64);
+			function(err, users) {
+				if (err) return console.error(err);
+				assert.equal( users.message.length, 72);
 			}
 		)
 }
 
-// Testing specific user
 function get_user() {
 	seneca
 		.client()
 		.act(
 			{action: 'get', username: 'nal2'},
-			function( err, result) {
+			function(err, result) {
+				if (err) return console.error(err);
 				assert.equal( result.message.team, 'SPDG');
 			}
 		)
 }
-	
-// Testing create user
+
 function create_user() {
 	seneca
 		.client()
 		.act(
 			{
 				action: 'add', 
-				name: 'Juan Carlos Faundez',
+				name: 'Juan Carlos Faundez 16',
 				birthday: '01/01/1990',
 				startDate: '21/07/2015',
 				position: 'Scrum Master Entry',
-				username: 'jcf10',
+				username: 'jcf16',
 				team: 'FRAUD',
 				gender: 'Masculino',
 				//password: '',
-				email: 'jcfaundez10@equifax.com'
+				email: 'jcfaundez16@equifax.com'
 			},
 			function (err, user_saved) {
+				if (err) return console.error(err);
 				this.act(
 					{action:'get', username: user_saved.message.username},
 					function(err, result) {
+						if (err) return console.error(err);
 						assert.equal(user_saved.message.username, result.message.username);
 					} 
 				)
